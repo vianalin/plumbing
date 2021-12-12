e66c138db1a43f526da89a69990c44b75bd9a1a9
Viana Lin <vianalin@pop-os.localdomain>
2021-12-12 15:28:50 -0500

    plumbing

--- plumb.c
+++ plumb.c
@@ -1,22 +1,71 @@
+#include <stdlib.h>
 #include <stdio.h>
 
+#include <unistd.h>
+#include <string.h>
+
+#define READ 0
+#define WRITE 1
+
 //design a program that uses unnamed pipes to facilitate parent/child process communication
-int plumbing() {
+int main() {
+
 	//in order to facilitate 2 way communication, you will need 2 pipes
-	int input[2];
-	int output[2];
-	pipe(input);
-	pipe(output);
 	
-	//parent
-		//constantly prompts a user for input 
-		//sends that input to child 
-		//waits for a response 
-		//displays the response to the user
+	//to child
+	int child[2];
+	pipe(child);
 	
-	//child
-		//gets input from parent
-		//processes the input: any of a number of string processes (all caps, all lower, reverse)
-		//sends the response to parent
+	//to parent
+	int parent[2];
+	pipe(parent);
+	
+	int f;
+	f = fork();
+	
+	if(f) {
+	
+		//parent
+		close(child[READ]);
+		close(parent[WRITE]);
 		
+		while(1) {
+		
+			//constantly prompts a user for input
+			printf("input: ");
+			
+			//sends that input to child 
+			char line[20];
+			fgets(line, 20, stdin);
+			write(child[WRITE], line, sizeof(line));
+			
+			//displays the response to the user
+			char s[20];
+			read(parent[READ], s, sizeof(s));
+			printf("response from child: %s\n", s);
+		}
+		
+	} else {
 	
+		//child
+		close(parent[READ]);
+		close(child[WRITE]);
+		
+		//gets input from parent
+		char s[20];
+		while(read(child[READ], s, sizeof(s))) {
+		
+		//processes the input: any of a number of string processes (all caps, all lower, reverse)
+			s[strlen(s) - 1] = '\0';
+			strcat(s, ":)");
+			s[strlen(s)] = '\0';
+			
+			//sends the response to parent
+			write(parent[WRITE], s, strlen(s) + 1);
+			s[0] = '\0';
+		}
+	}
+
+	return 0;	
+}
+
